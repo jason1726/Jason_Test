b2abde9ad3db81ca0416c51257e58b8ea7d9de9c
jason1726 <jayjoycer@gmail.com>
2017-06-11 13:17:37 +0800

    建置MVC畫面

---
+++ Employee/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Employee.Models
+{
+    public class Employee
+    {
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public string EmpTel { get; set; }
+        public string EmpAddress { get; set; }
+    }
+
+    public class EmpDBContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
